dbe074de50ea45c58561439e1bdf84e87e50ece0
Rodrigo Coria <coriarodrigoc@gmail.com>
2015-09-30 18:48:58 -0300

    Usuarios

---
+++ BusquedaLaGaceta/Gui/Usuarios.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BusquedaLaGaceta.Gui
+{
+    public partial class Usuarios : Form
+    {
+        public Usuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void Usuarios_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'aIDataSet.Table_User' table. You can move, or remove it, as needed.
+            this.table_UserTableAdapter.Fill(this.aIDataSet.Table_User);
+            // TODO: This line of code loads data into the 'aIDataSet.Table_User' table. You can move, or remove it, as needed.            
+
+        }
+
+        private void btnAlta_Click(object sender, EventArgs e)
+        {
+            UsuarioAM usuarios = new UsuarioAM();
+            usuarios.Show();  
+        }
+    }
+}
